d0ccc6325b95b662a8247666bc38bfdaf6178717
Sudeep Reddy <sudeep19313@iiitd.ac.in>
2021-03-09 03:19:38 +0530

    new assets and animations

---
+++ Website/js/screen2_assets.js
@@ -0,0 +1,3 @@
+var SPLINE_ASSETS_LIST_2 = { fonts: [], images: [], models: [], animations: [] }
+var SPLINE_ASSETS_LIST_3 = { fonts: [], images: [], models: [], animations: [] }
+var SPLINE_ASSETS_LIST_4 = { fonts: [], images: [], models: [], animations: [] }
---
+++ Website/js/screen2_main.js
@@ -0,0 +1,11 @@
+
+            var SPE_USES_PREVIEW_IMAGE_2 = false;
+            const runtime2 = new SpeRuntime( "canvas3d",SPLINE_EXPORTED_SCENE_2, SPLINE_ASSETS_LIST_2);
+            runtime2.run();
+            var SPE_USES_PREVIEW_IMAGE_3 = false;
+            const runtime3 = new SpeRuntime( "game",SPLINE_EXPORTED_SCENE_3, SPLINE_ASSETS_LIST_3 );
+            runtime3.run();
+            var SPE_USES_PREVIEW_IMAGE_4 = false;
+            const runtime4 = new SpeRuntime( "flutter",SPLINE_EXPORTED_SCENE_4, SPLINE_ASSETS_LIST_4 );
+            runtime4.run();
+        
