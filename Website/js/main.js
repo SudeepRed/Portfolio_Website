850d48ac8b2bc7a2349e35b2cef99eaa04b9b8fd
Sudeep Reddy <sudeep19313@iiitd.ac.in>
2021-03-09 10:18:07 +0530

    performance improvements

--- Website/js/main.js
+++ Website/js/main.js
@@ -1,6 +1,6 @@
 
             var SPE_USES_PREVIEW_IMAGE = false;
-            const runtime = new SpeRuntime( "gg",SPLINE_EXPORTED_SCENE, SPLINE_ASSETS_LIST );
-            runtime.run();
+            // const runtime = new SpeRuntime( "gg",SPLINE_EXPORTED_SCENE, SPLINE_ASSETS_LIST );
+            // runtime.run();
 
         
--- Website/js/screen2_main.js
+++ Website/js/screen2_main.js
@@ -1,11 +1,16 @@
 
-            var SPE_USES_PREVIEW_IMAGE_2 = false;
-            const runtime2 = new SpeRuntime( "canvas3d",SPLINE_EXPORTED_SCENE_2, SPLINE_ASSETS_LIST_2);
-            runtime2.run();
-            var SPE_USES_PREVIEW_IMAGE_3 = false;
-            const runtime3 = new SpeRuntime( "game",SPLINE_EXPORTED_SCENE_3, SPLINE_ASSETS_LIST_3 );
-            runtime3.run();
-            var SPE_USES_PREVIEW_IMAGE_4 = false;
-            const runtime4 = new SpeRuntime( "flutter",SPLINE_EXPORTED_SCENE_4, SPLINE_ASSETS_LIST_4 );
-            runtime4.run();
+            let runtime = new SpeRuntime( "gg",SPLINE_EXPORTED_SCENE, SPLINE_ASSETS_LIST );
+            runtime.run();
+            runtime = new SpeRuntime( "canvas3d",SPLINE_EXPORTED_SCENE_2, SPLINE_ASSETS_LIST_2);
+            runtime.run();
+            runtime = new SpeRuntime( "game",SPLINE_EXPORTED_SCENE_3, SPLINE_ASSETS_LIST_3 );
+            runtime.run();
+            runtime = new SpeRuntime( "flutter",SPLINE_EXPORTED_SCENE_4, SPLINE_ASSETS_LIST_4 );
+            runtime.run();
+            // var SPE_USES_PREVIEW_IMAGE_3 = false;
+            // const runtime3 = new SpeRuntime( "game",SPLINE_EXPORTED_SCENE_3, SPLINE_ASSETS_LIST_3 );
+            // runtime3.run();
+            // var SPE_USES_PREVIEW_IMAGE_4 = false;
+            // const runtime4 = new SpeRuntime( "flutter",SPLINE_EXPORTED_SCENE_4, SPLINE_ASSETS_LIST_4 );
+            // runtime4.run();
         
