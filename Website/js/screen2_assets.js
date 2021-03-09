2e353f59ced2222ccc86aebd958291afe30ad2c3
Sudeep Reddy <sudeep19313@iiitd.ac.in>
2021-03-09 23:09:56 +0530

    changed these and added assets

--- Website/js/screen2_assets.js
+++ Website/js/screen2_assets.js
@@ -1,3 +1,5 @@
 var SPLINE_ASSETS_LIST_2 = { fonts: [], images: [], models: [], animations: [] }
 var SPLINE_ASSETS_LIST_3 = { fonts: [], images: [], models: [], animations: [] }
 var SPLINE_ASSETS_LIST_4 = { fonts: [], images: [], models: [], animations: [] }
+var SPLINE_ASSETS_LIST_5 = { fonts: [], images: [], models: [], animations: [] }
+var SPLINE_ASSETS_LIST_6 = { fonts: [], images: [], models: [], animations: [] }
--- Website/js/screen2_main.js
+++ Website/js/screen2_main.js
@@ -1,11 +1,15 @@
 
-            let runtime = new SpeRuntime( "gg",SPLINE_EXPORTED_SCENE, SPLINE_ASSETS_LIST );
-            runtime.run();
-            runtime = new SpeRuntime( "canvas3d",SPLINE_EXPORTED_SCENE_2, SPLINE_ASSETS_LIST_2);
-            runtime.run();
-            runtime = new SpeRuntime( "game",SPLINE_EXPORTED_SCENE_3, SPLINE_ASSETS_LIST_3 );
+            // let runtime = new SpeRuntime( "gg",SPLINE_EXPORTED_SCENE, SPLINE_ASSETS_LIST );
+            // runtime.run();
+            // runtime = new SpeRuntime( "canvas3d",SPLINE_EXPORTED_SCENE_2, SPLINE_ASSETS_LIST_2);
+            // runtime.run();
+            let runtime = new SpeRuntime( "game",SPLINE_EXPORTED_SCENE_3, SPLINE_ASSETS_LIST_3 );
             runtime.run();
             runtime = new SpeRuntime( "flutter",SPLINE_EXPORTED_SCENE_4, SPLINE_ASSETS_LIST_4 );
+            runtime.run();
+            runtime = new SpeRuntime( "canvas3d",SPLINE_EXPORTED_SCENE_5, SPLINE_ASSETS_LIST_5 );
+            runtime.run();
+            runtime = new SpeRuntime( "gg",SPLINE_EXPORTED_SCENE_6, SPLINE_ASSETS_LIST_6 );
             runtime.run();
             // var SPE_USES_PREVIEW_IMAGE_3 = false;
             // const runtime3 = new SpeRuntime( "game",SPLINE_EXPORTED_SCENE_3, SPLINE_ASSETS_LIST_3 );
